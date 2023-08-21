1a5a765784d797647de02cc5687df84dda9dc8b6
Saurav <saurav.ronaldo7@gmail.com>
2023-08-21 20:48:45 +1000

    arithemetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let random;
+let mod;
+let max;
+let num1 = 2;
+let num2 = 31;
+let num3 = 19;
+let num4 = 5;
+
+
+multiply = num1 * num2
+random = Math.floor(Math.random()) + 1
+mod = num3 % num4
+max = Math.max(1,3,5,20)
